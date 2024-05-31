f341baeef3f66aa40e1a1885fa8e584fd8f77d51
francooooo12 <andreacarizzoni05@gmail.com>
2024-05-31 12:13:25 +0200

    fix

--- database.js
+++ database.js
@@ -2,7 +2,7 @@
     const { HOST, USERNAME, PASSWORD, DBNAME, PORT } = process.env;
     console.log();
     const connpool = mysql.createPool({
-        host: "10.20.2.172",
+        host: "10.20.2.173",
         user: "tps",
         password: "ttppss",
         database: "gamification"
